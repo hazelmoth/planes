d353ab2defdf67217f67908e5e33832458d2a661
linuswaddell <24817932+linuswaddell@users.noreply.github.com>
2022-05-01 16:29:56 -0700

    improved bf 109 flight characteristics

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using MFlight;
 using UnityEngine;
@@ -6,38 +7,44 @@ using TMPro;
 
 public class HUDController : MonoBehaviour
 {
-	[SerializeField] TextMeshProUGUI speedText;
-	[SerializeField] TextMeshProUGUI vertSpeedText;
-	[SerializeField] TextMeshProUGUI throttleText;
-	[SerializeField] TextMeshProUGUI altitudeText;
-	[SerializeField] TextMeshProUGUI dragAmountText;
-	[SerializeField] TextMeshProUGUI angleOfAttackText;
-	[SerializeField] TextMeshProUGUI pitchText;
-	[SerializeField] TextMeshProUGUI rollText;
-	[SerializeField] TextMeshProUGUI headingText;
-	[SerializeField] MouseFlightController input;
+	[SerializeField] private TextMeshProUGUI speedText;
+	[SerializeField] private TextMeshProUGUI vertSpeedText;
+	[SerializeField] private TextMeshProUGUI throttleText;
+	[SerializeField] private TextMeshProUGUI thrustText;
+	[SerializeField] private TextMeshProUGUI altitudeText;
+	[SerializeField] private TextMeshProUGUI dragAmountText;
+	[SerializeField] private TextMeshProUGUI inducedDragText;
+	[SerializeField] private TextMeshProUGUI parasiticDragText;
+	[SerializeField] private TextMeshProUGUI angleOfAttackText;
+	[SerializeField] private TextMeshProUGUI pitchText;
+	[SerializeField] private TextMeshProUGUI rollText;
+	[SerializeField] private TextMeshProUGUI headingText;
+	[SerializeField] private MouseFlightController input;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
 		if (input.ActivePlane != null)
 		{
-			speedText.text = System.Math.Round(input.ActivePlane.CurrentSpeed, 1) + " m/s";
-			altitudeText.text = "altitude: " + System.Math.Round(input.ActivePlane.Altitude, 0) + " m";
-			vertSpeedText.text = "vert: " + System.Math.Round(input.ActivePlane.VerticalSpeed, 1) + " m/s";
-			dragAmountText.text = "drag:" + System.Math.Round(input.ActivePlane.DragMagnitude, 0) + " N";
-			angleOfAttackText.text = System.Math.Round(input.ActivePlane.AngleOfAttack, 1)
+			speedText.text = Math.Round(input.ActivePlane.CurrentSpeed,                                  1) + " m/s";
+			vertSpeedText.text = "vert: " + Math.Round(input.ActivePlane.VerticalSpeed,                  1) + " m/s";
+			thrustText.text = "thrust: " + Math.Round(input.ActivePlane.ThrustMagnitude,                 1) + " N";
+			altitudeText.text = "altitude: " + Math.Round(input.ActivePlane.Altitude,                    0) + " m";
+			dragAmountText.text = "drag:" + Math.Round(input.ActivePlane.DragMagnitude,                  0) + " N";
+			inducedDragText.text = "induced:" + Math.Round(input.ActivePlane.InducedDragMagnitude,       0) + " N";
+			parasiticDragText.text = "parasitic:" + Math.Round(input.ActivePlane.ParasiticDragMagnitude, 0) + " N";
+			angleOfAttackText.text = Math.Round(input.ActivePlane.AngleOfAttack, 1)
 				+ "° AoA, "
-				+ System.Math.Round(input.ActivePlane.SideslipAngle, 1)
+				+ Math.Round(input.ActivePlane.SideslipAngle, 1)
 				+ "° sideslip";
-			pitchText.text = "pitch: " + System.Math.Round(input.ActivePlane.Pitch,       1) + "°";
-			rollText.text = "roll: " + System.Math.Round(input.ActivePlane.Roll,          1) + "°";
-			headingText.text = "heading: " + System.Math.Round(input.ActivePlane.Heading, 1) + "°";
+			pitchText.text = "pitch: " + Math.Round(input.ActivePlane.Pitch,       1) + "°";
+			rollText.text = "roll: " + Math.Round(input.ActivePlane.Roll,          1) + "°";
+			headingText.text = "heading: " + Math.Round(input.ActivePlane.Heading, 1) + "°";
 		}
 		throttleText.text = Mathf.Round(input.CurrentThrottle * 100) + "% throttle";
     }
--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -12,28 +12,25 @@ public class PlaneController : MonoBehaviour
 	[SerializeField] Vector3 centerOfDrag;
 	[SerializeField] AnimationCurve liftCurve;
 
-	Rigidbody rigidbody;
+	private Rigidbody rigidbody;
 
 
-	[SerializeField] private float maxThrust = 150000;
+	[SerializeField] private float enginePowerWatts = 800000;
+	[SerializeField] private float staticThrust = 15000;
+
 	[SerializeField] private float liftConstant = 20f;
 	[SerializeField] private float optimalDragConstant = 10f;
 	[SerializeField] private float backwardsDragConstant = 40f;
 	[SerializeField] private float inducedDragConstant = 10f;
 	[SerializeField] private float sidewaysLiftMultiplier = 0.25f;
 
-
-	// in N
-	[SerializeField] private float yawForce = 3000f;
-	[SerializeField] private float pitchForce = 10000f;
-
 	[SerializeField] private float rollTorqueConstant = 4f;
 	[SerializeField] private float pitchTorqueConstant = 3f;
 	[SerializeField] private float yawTorqueConstant = 1f;
 
-	float currentRudderAmount;
-	float currentAileronAmount;
-	float currentElevatorAmount;
+	private float currentRudderAmount;
+	private float currentAileronAmount;
+	private float currentElevatorAmount;
 
 	public PlaneWeapons Weapons => weapons;
 
@@ -65,6 +62,19 @@ public class PlaneController : MonoBehaviour
 		}
 	}
 
+	/// The aircraft's velocity vector projected onto its forward vector.
+	public Vector3 ForwardVelocity => Vector3.Project(rigidbody.velocity, transform.forward);
+
+	public float DragMagnitude { get; private set; }
+
+	public float InducedDragMagnitude => CalculateInducedDragMagnitude();
+
+	public float ParasiticDragMagnitude => CalculateParasiticDragMagnitude();
+
+	public float ThrustMagnitude => CalculateThrustVector().magnitude;
+
+	public float CurrentThrottle { get; private set; }
+
 	/// The angle between the velocity vector and forward vector as projected on the
 	/// aircraft's XZ plane.
 	public float SideslipAngle
@@ -79,11 +89,10 @@ public class PlaneController : MonoBehaviour
 		}
 	}
 
-	public float DragMagnitude { get; private set; }
-	public float CurrentThrottle { get; private set; }
 	public bool GearDeployed =>
 		gear.State is LandingGearController.GearState.Deployed
 			or LandingGearController.GearState.Deploying;
+
 
 	// Controls
 	public void SetThrottle(float power) =>
@@ -123,12 +132,11 @@ public class PlaneController : MonoBehaviour
 	}
 
 	// Update is called once per frame
-	void FixedUpdate()
+	private void FixedUpdate()
     {
 		if (CurrentThrottle < 0) CurrentThrottle = 0;
 
-		float thrust = maxThrust * CurrentThrottle;
-		Vector3 thrustVector = Vector3.forward * thrust;
+		Vector3 thrustVector = CalculateThrustVector();
 
 		Vector3 liftVector = Vector3.up * CalculateLift();
 		liftVector += Vector3.right * CalculateSidewaysLift();
@@ -136,7 +144,6 @@ public class PlaneController : MonoBehaviour
 		Vector3 rollTorque = Vector3.back * CalculateRollTorque();
 		Vector3 yawTorque = Vector3.up * CalculateYawTorque();
 		Vector3 pitchTorque = Vector3.left * CalculatePitchTorque();
-
 		Vector3 netTorque = yawTorque + rollTorque + pitchTorque;
 
 		Vector3 netForce = thrustVector + liftVector;
@@ -157,9 +164,16 @@ public class PlaneController : MonoBehaviour
 
     }
 
+	private Vector3 CalculateThrustVector()
+	{
+		float thrust = ForwardVelocity.magnitude > 0f
+			? CurrentThrottle * enginePowerWatts / (ForwardVelocity.magnitude)
+			: float.MaxValue;
+		thrust = Mathf.Min(staticThrust, thrust);
+		return Vector3.forward * thrust;
+	}
 
-
-	float CalculateLift () {
+	private float CalculateLift () {
 		// in N
 		float pitchFactor = liftCurve.Evaluate(AngleOfAttack / 180);
 		float liftForce = liftConstant * Mathf.Pow(CurrentSpeed, 2) * pitchFactor;
@@ -177,23 +191,33 @@ public class PlaneController : MonoBehaviour
 
 	private float CalculateRollTorque()
 	{
-		Vector3 forwardVelocity = Vector3.Project(rigidbody.velocity, transform.forward);
-		return Mathf.Pow(forwardVelocity.magnitude, 2) * rollTorqueConstant * currentAileronAmount;
+		return Mathf.Pow(ForwardVelocity.magnitude, 2) * rollTorqueConstant * currentAileronAmount;
 	}
 
 	private float CalculatePitchTorque()
 	{
-		Vector3 forwardVelocity = Vector3.Project(rigidbody.velocity, transform.forward);
-		return Mathf.Pow(forwardVelocity.magnitude, 2) * pitchTorqueConstant * currentElevatorAmount;
+		return Mathf.Pow(ForwardVelocity.magnitude, 2) * pitchTorqueConstant * currentElevatorAmount;
 	}
 
 	private float CalculateYawTorque()
 	{
-		Vector3 forwardVelocity = Vector3.Project(rigidbody.velocity, transform.forward);
-		return Mathf.Pow(forwardVelocity.magnitude, 2) * yawTorqueConstant * currentRudderAmount;
+		return Mathf.Pow(ForwardVelocity.magnitude, 2) * yawTorqueConstant * currentRudderAmount;
 	}
 
-	Vector3 CalculateDragVector ()
+	private Vector3 CalculateDragVector ()
+	{
+		float parasiticDragMagnitude = CalculateParasiticDragMagnitude();
+		float inducedDragMagnitude = CalculateInducedDragMagnitude();
+		DragMagnitude = inducedDragMagnitude + parasiticDragMagnitude;
+
+		Vector3 velocity = transform.InverseTransformVector(rigidbody.velocity);
+		Vector3 trajectory = velocity.normalized;
+		Vector3 dragTrajectory = -trajectory;
+
+		return dragTrajectory * DragMagnitude;
+	}
+
+	private float CalculateParasiticDragMagnitude ()
 	{
 		Vector3 velocity = transform.InverseTransformVector(rigidbody.velocity);
 		Vector3 trajectory = velocity.normalized;
@@ -205,13 +229,17 @@ public class PlaneController : MonoBehaviour
 		float dragFactor = (dragAngleFactor * (backwardsDragConstant - optimalDragConstant) + optimalDragConstant);
 
 		float parasiticDragMagnitude = dragFactor * Mathf.Pow(velocity.magnitude, 2);
-		float inducedDragMagnitude = AngleOfAttack * inducedDragConstant;
 
-		if (velocity.magnitude == 0f)
+		return parasiticDragMagnitude;
+	}
+
+	private float CalculateInducedDragMagnitude ()
+	{
+		float inducedDragMagnitude = CalculateLift() * inducedDragConstant / Mathf.Pow(rigidbody.velocity.magnitude, 4);
+
+		if (ForwardVelocity.magnitude <= 20f)
 			inducedDragMagnitude = 0;
 
-		DragMagnitude = inducedDragMagnitude + parasiticDragMagnitude;
-
-		return dragTrajectory * DragMagnitude;
+		return inducedDragMagnitude;
 	}
 }
